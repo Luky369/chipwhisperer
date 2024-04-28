ffa4c02bbad519d73e0bcee19083bbcc64b61381
Luky369 <ludanekas@gmail.com>
2024-04-28 16:06:45 +0200

    adding chacha files from openssl and tested on PC

--- hardware/victims/firmware/crypto/chacha/chacha.c
+++ hardware/victims/firmware/crypto/chacha/chacha.c
@@ -1,170 +1,99 @@
-/*
-   SipHash reference C implementation
-
-   Copyright (c) 2012-2021 Jean-Philippe Aumasson
-   <jeanphilippe.aumasson@gmail.com>
-   Copyright (c) 2012-2014 Daniel J. Bernstein <djb@cr.yp.to>
-
-   To the extent possible under law, the author(s) have dedicated all copyright
-   and related and neighboring rights to this software to the public domain
-   worldwide. This software is distributed without any warranty.
-
-   You should have received a copy of the CC0 Public Domain Dedication along
-   with
-   this software. If not, see
-   <http://creativecommons.org/publicdomain/zero/1.0/>.
- */
-
-#include "chacha.h"
-#include <assert.h>
-#include <stdint.h>
 #include <stdio.h>
+#include <stdint.h>
+#include "openssl_endian.h"
+#include "openssl_chacha.h"
 
-#undef DEBUG
 
-/* default: SipHash-2-4 */
-#ifndef cROUNDS
-#define cROUNDS 2
-#endif
-#ifndef dROUNDS
-#define dROUNDS 4
-#endif
+#define LEN 64
 
-#define ROTL(x, b) (uint64_t)(((x) << (b)) | ((x) >> (64 - (b))))
+#define KEY_LEN_BYTES 32
 
-#define U32TO8_LE(p, v)                                                        \
-    (p)[0] = (uint8_t)((v));                                                   \
-    (p)[1] = (uint8_t)((v) >> 8);                                              \
-    (p)[2] = (uint8_t)((v) >> 16);                                             \
-    (p)[3] = (uint8_t)((v) >> 24);
+#define WORD_BYTE_COUNT 4
 
-#define U64TO8_LE(p, v)                                                        \
-    U32TO8_LE((p), (uint32_t)((v)));                                           \
-    U32TO8_LE((p) + 4, (uint32_t)((v) >> 32));
+#define KEY_LEN_WORDS KEY_LEN_BYTES / WORD_BYTE_COUNT
 
-#define U8TO64_LE(p)                                                           \
+#define COUNTER_CONST 0x01
+#define NONCE_0 0
+#define NONCE_1 0x4a
+#define NONCE_2 0x09
+
+
+
+#define U8TO32_LE(p)                                                           \
     (((uint64_t)((p)[0])) | ((uint64_t)((p)[1]) << 8) |                        \
-     ((uint64_t)((p)[2]) << 16) | ((uint64_t)((p)[3]) << 24) |                 \
-     ((uint64_t)((p)[4]) << 32) | ((uint64_t)((p)[5]) << 40) |                 \
-     ((uint64_t)((p)[6]) << 48) | ((uint64_t)((p)[7]) << 56))
+     ((uint64_t)((p)[2]) << 16) | ((uint64_t)((p)[3]) << 24))
 
-#define SIPROUND                                                               \
-    do {                                                                       \
-        v0 += v1;                                                              \
-        v1 = ROTL(v1, 13);                                                     \
-        v1 ^= v0;                                                              \
-        v0 = ROTL(v0, 32);                                                     \
-        v2 += v3;                                                              \
-        v3 = ROTL(v3, 16);                                                     \
-        v3 ^= v2;                                                              \
-        v0 += v3;                                                              \
-        v3 = ROTL(v3, 21);                                                     \
-        v3 ^= v0;                                                              \
-        v2 += v1;                                                              \
-        v1 = ROTL(v1, 17);                                                     \
-        v1 ^= v2;                                                              \
-        v2 = ROTL(v2, 32);                                                     \
-    } while (0)
+
+#define U8TO32_BE(p)                                                           \
+    (((uint32_t)((p)[0]) << 24) | ((uint32_t)((p)[1]) << 16) |                \
+     ((uint32_t)((p)[2]) << 8) | ((uint32_t)((p)[3])))
+
+
+
+void chacha(const void *in, const size_t inlen, const void *k, uint8_t *out,
+            const size_t outlen) {
+	
+	unsigned int counter[4] = {COUNTER_CONST,NONCE_2,NONCE_1,NONCE_0};
+	const unsigned char *in_uc = (const unsigned char *)in;
+    const unsigned char *k_uc  = (const unsigned char *)k;
+	unsigned int k_ui[8];
+
+
+	DECLARE_IS_ENDIAN;
+
+	for (size_t i = 0; i < KEY_LEN_WORDS; ++i) {
+		if (IS_LITTLE_ENDIAN) {
+			k_ui[i] = U8TO32_LE(k_uc + i * WORD_BYTE_COUNT);
+		} else {
+			k_ui[i] = U8TO32_BE(k_uc + i * WORD_BYTE_COUNT);		
+		}
+    }
 
 #ifdef DEBUG
-#define TRACE                                                                  \
-    do {                                                                       \
-        printf("(%3zu) v0 %016" PRIx64 "\n", inlen, v0);                       \
-        printf("(%3zu) v1 %016" PRIx64 "\n", inlen, v1);                       \
-        printf("(%3zu) v2 %016" PRIx64 "\n", inlen, v2);                       \
-        printf("(%3zu) v3 %016" PRIx64 "\n", inlen, v3);                       \
-    } while (0)
-#else
-#define TRACE
+    printf("\nKEY - int: ");
+    for (size_t i = 0; i < KEY_LEN_WORDS; ++i) {
+        printf("%08X ",k_ui[i]);
+    }
+    printf("\n");
 #endif
+	ChaCha20_ctr32((unsigned char *)out, in_uc, inlen, k_ui, counter);
 
-int siphash(const void *in, const size_t inlen, const void *k, uint8_t *out,
-            const size_t outlen) {
+}
 
-    const unsigned char *ni = (const unsigned char *)in;
-    const unsigned char *kk = (const unsigned char *)k;
 
-    uint64_t v0 = UINT64_C(0x736f6d6570736575);
-    uint64_t v1 = UINT64_C(0x646f72616e646f6d);
-    uint64_t v2 = UINT64_C(0x6c7967656e657261);
-    uint64_t v3 = UINT64_C(0x7465646279746573);
-    uint64_t k0 = U8TO64_LE(kk);
-    uint64_t k1 = U8TO64_LE(kk + 8);
-    uint64_t m;
-    int i;
-    const unsigned char *end = ni + inlen - (inlen % sizeof(uint64_t));
-    const int left = inlen & 7;
-    uint64_t b = ((uint64_t)inlen) << 56;
-    v3 ^= k1;
-    v2 ^= k0;
-    v1 ^= k1;
-    v0 ^= k0;
+#ifdef PC_DEBUG
+int main (int argc, char *argv[]) {
+    // ChaCha20_ctr32(unsigned char *out, const unsigned char *inp, size_t len,
+                    // const unsigned int key[8], const unsigned int counter[4])
+    unsigned char out[LEN];
+    unsigned char inp[LEN];
+    size_t len = LEN;
 
-    if (outlen == 16)
-        v1 ^= 0xee;
+    uint8_t key[KEY_LEN_BYTES] = {
+        0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+        0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,
+        0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17,
+        0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f
+    };
 
-    for (; ni != end; ni += 8) {
-        m = U8TO64_LE(ni);
-        v3 ^= m;
-
-        TRACE;
-        for (i = 0; i < cROUNDS; ++i)
-            SIPROUND;
-
-        v0 ^= m;
+    for (size_t i = 0; i < LEN; ++i) {
+        inp[i] = 0x01;
     }
+    
+    chacha(inp, len, key, out, len);
 
-    switch (left) {
-    case 7:
-        b |= ((uint64_t)ni[6]) << 48;
-    case 6:
-        b |= ((uint64_t)ni[5]) << 40;
-    case 5:
-        b |= ((uint64_t)ni[4]) << 32;
-    case 4:
-        b |= ((uint64_t)ni[3]) << 24;
-    case 3:
-        b |= ((uint64_t)ni[2]) << 16;
-    case 2:
-        b |= ((uint64_t)ni[1]) << 8;
-    case 1:
-        b |= ((uint64_t)ni[0]);
-        break;
-    case 0:
-        break;
+    printf("INP: ");
+    for (size_t i = 0; i < LEN; ++i) {
+        printf("%02X ",inp[i]);
     }
+    printf("\n");
 
-    v3 ^= b;
-
-    TRACE;
-    for (i = 0; i < cROUNDS; ++i)
-        SIPROUND;
-
-    v0 ^= b;
-
-    if (outlen == 16)
-        v2 ^= 0xee;
-    else
-        v2 ^= 0xff;
-
-    TRACE;
-    for (i = 0; i < dROUNDS; ++i)
-        SIPROUND;
-
-    b = v0 ^ v1 ^ v2 ^ v3;
-    U64TO8_LE(out, b);
-
-    if (outlen == 8)
-        return 0;
-
-    v1 ^= 0xdd;
-
-    TRACE;
-    for (i = 0; i < dROUNDS; ++i)
-        SIPROUND;
-
-    b = v0 ^ v1 ^ v2 ^ v3;
-    U64TO8_LE(out + 8, b);
-
+    printf("OUT: ");
+    for (size_t i = 0; i < LEN; ++i) {
+        printf("%02X ",out[i]);
+    }
+    printf("\n");
+    
     return 0;
 }
+#endif //PC_DEBUG
